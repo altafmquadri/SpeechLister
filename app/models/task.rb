c318ad16234dcd5f22136bec2b589f5ec9dd86e0
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-13 18:37:01 -0500

    added the new endpoint

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,38 @@ class Task < ApplicationRecord
     Task.all.sort_by(&:due_date)
   end
 
+  # def self.by_date
+  #   uniq_due = Task.all.map{|task| task.due_date}.uniq
+  #   uniq_due.map{|due_date| {due_date => Task.all.select{|task| task.due_date == due_date}}}
+  # end
+
+  # def self.by_date_user(username)
+  #   user_tasks = Task.all.select{|task| task.user.username == username}
+  #   uniq_dates = user_tasks.map(&:due_date).uniq
+  #   tasks_per_date_per_user = uniq_dates.map{|task| username => []}
+  # end
+
 end
+
+# user_tasks = User.all.map{|user| user.tasks}
+# users_due_dates = user_tasks.map(&:due_date).uniq
+# user_tasks.map{|task| task.user.username => users_due_dates.map{|due_date|{due_date => Task.all.select{|task| task.due_date == due_date}}}}
+
+# [
+#   {
+#     due_date: [
+#       task1, task2, task3
+#     ]
+#   }
+# ]
+
+# [
+#   altaf: [
+#     {2019-10-10: [
+#       task1, task2, task3
+#     ]},
+#     {2018-12-41: [
+
+#     ]}
+#   ]
+# ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
     has_many :tasks
     has_many :lists, through: :tasks
+
+    def self.tasks_by_date
+
+        all_tasks = User.all.map(&:tasks).flatten
+        all_uniq_dates = all_tasks.map(&:due_date).uniq
+        
+        all_tasks.map do |task|
+            {"#{task.user.username}" => all_uniq_dates.map do |due_date|
+                {"#{due_date}" => all_tasks.select do |task|
+                    task.due_date == due_date
+                end.sort_by{|task| task.completed ? 0 : 1}.reverse}
+            end}
+        end.uniq
+    end
 end
